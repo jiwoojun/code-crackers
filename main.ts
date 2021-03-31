fbc5ad6df54ec795484ff94ee8155378f3c44f33
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2021-04-01 10:32:00 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let numCode = game.askForNumber("crack this code!")
+if (numCode == 794) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 222) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 803) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 333) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 101) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 198) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 183) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 157) {
+    numCode = game.askForNumber("crack this code!")
+}
+if (numCode == 999) {
+    game.over(true)
+}
